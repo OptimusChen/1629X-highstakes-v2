51aad3201f9738562bc05786b1bf0527c9d6fbac
OptimusChen <derek.hz.chen@gmail.com>
2025-03-01 10:54:22 -0800

    logging lib

--- include/lib/logging.hpp
+++ include/lib/logging.hpp
@@ -2,9 +2,12 @@
 #include <array>
 #include <string>
 #include <vector>
+#include "pros/rtos.hpp"
+#include <iostream>
+#include <fstream>
+#include <cstdio>
 
-#define MAX_LOG_ENTRIES 10000  // Fixed circular buffer size
-#define MAX_LOGGERS 10  // Fixed circular buffer size
+#define MAX_LOGGERS 4  // Fixed circular buffer size
 #define LOG_SIZE 4  // Fixed circular buffer size
 
 /**
@@ -23,33 +26,95 @@ enum class LogType {
  */
 struct LogElement {
     LogType type;        // Type of the log
-    uint32_t time;          // Timestamp
+    int time;          // Timestamp
     float a;             // X position (or relevant data)
     float b;             // Y position (or relevant data)
     float c;       // Heading (or relevant data)
     float d;       // Heading (or relevant data)
 };
 
-std::array<Logger, MAX_LOGGERS> all_loggers;
-size_t logger_count = 0;  // Total number of log entries across all loggers
+// Collection of all logger instances
+extern bool writingToFile;
 
 /**
  * Logger class for managing log entries
  */
+template <size_t MAX_LOG_ENTRIES>
 class Logger {
 private:
     std::array<LogElement, MAX_LOG_ENTRIES> logs; // Fixed-size circular buffer
     size_t log_index = 0;  // Circular buffer index
     size_t valid_count = 0;
+    bool enabled = true;
     std::string logger_name; // Unique name for identifying the logger instance
 
 public:
-    /** Constructor that registers the instance */
-    Logger(const std::string& name);
+    /**
+     * Register logger instances globally (Called in constructor)
+     */
+    Logger(const std::string& name, const bool _enabled = true) : logger_name(name), enabled(_enabled) {
+    }
 
-    /** Push a log entry into the buffer */
-    void push_log(LogType type, std::array<float, LOG_SIZE> data);
+    void enable(const bool _enabled) {
+        enabled = _enabled;
+    }
 
-    /** Dump all logs from all instances to a single file */
-    static void dump_all();
+    /**
+     * Push a log entry into the buffer
+     * @param type The log type
+     * @param data The data to log (x, y, heading, time)
+     * This is a fixed-size array of 4 elements
+     * The data is stored in a circular buffer fashion
+     * The log index wraps around when it reaches the maximum size
+     */
+    void push_log(LogType type, std::array<float, LOG_SIZE> data) {
+        if (writingToFile || !enabled) return;
+        logs[log_index] = {type, int(pros::millis()), data[0], data[1], data[2], data[3]};  // Store the log entry
+        log_index = (log_index + 1) % MAX_LOG_ENTRIES;  // Circular wrap-around
+        valid_count++;
+        if (valid_count > MAX_LOG_ENTRIES) valid_count = MAX_LOG_ENTRIES;  // Ensure valid count does not exceed max
+    }
+
+    void dump_file(FILE* log_file) {
+        char log_string[256];
+
+        for (size_t i = 0; i < valid_count; i++) {
+            const LogElement& log = logs[i];
+
+            switch (log.type) {
+                case LogType::POSITION_EXPECTED:
+                    sprintf(log_string, "%s:, Expected, %d, %f, %f, %f\n", logger_name.c_str(), log.time, log.a, log.b, log.c);
+                    break;
+                case LogType::POSITION_REAL:
+                    sprintf(log_string, "%s, Real, %d, %f, %f, %f\n", logger_name.c_str(), log.time, log.a, log.b, log.c);
+                    break;
+                case LogType::LOOP_TIME:
+                    sprintf(log_string, "%s, LoopTime, %d, %f\n", logger_name.c_str(), log.time, log.a);
+                    break;
+                case LogType::DISTANCE_SENSOR:
+                    sprintf(log_string, "%s, DistanceSensor, %d, %f, %f, %f, %f\n", logger_name.c_str(), log.time, log.a, log.b, log.c, log.d);
+                    break;
+                case LogType::DELTA_MOVEMENT:
+                    sprintf(log_string, "%s, DeltaMovement, %d, %f, %f\n", logger_name.c_str(), log.time, log.a, log.b);
+                    break;
+                default:
+                    break;
+            }
+
+            fputs(log_string, log_file);
+        }
+
+        std::cout << "Dumped logs for " << logger_name << std::endl;
+    }
 };
+
+extern void dump_all();
+
+constexpr size_t distanceLogSize = 140000;
+constexpr size_t robotLogSize = 70000;
+constexpr size_t autonLogSize = 1000;
+
+extern Logger<distanceLogSize> distanceLogger;
+extern Logger<robotLogSize> robotLogger;
+extern Logger<autonLogSize> autonLogger;
+
--- src/lib/logging.cpp
+++ src/lib/logging.cpp
@@ -6,41 +6,17 @@
 
 #include "pros/rtos.hpp"
 
-// Collection of all logger instances
-static std::array<Logger&, MAX_LOGGERS> all_loggers;
-
-/**
- * Register logger instances globally (Called in constructor)
- */
-Logger::Logger(const std::string& name) : logger_name(name) {
-    all_loggers.at(logger_count) = *this;  // Register this instance
-    logger_count++;
-    if (logger_count >= MAX_LOGGERS) {
-        std::cout << "Maximum number of loggers reached. Cannot register more." << std::endl;
-    }
-}
-
-/**
- * Push a log entry into the buffer
- * @param type The log type
- * @param data The data to log (x, y, heading, time)
- * This is a fixed-size array of 4 elements
-  * The data is stored in a circular buffer fashion
-  * The log index wraps around when it reaches the maximum size
- */
-void Logger::push_log(LogType type, std::array<float, LOG_SIZE> data) {
-    logs[log_index] = {type, pros::millis(), data[0], data[1], data[2], data[3]};  // Store the log entry
-    log_index = (log_index + 1) % MAX_LOG_ENTRIES;  // Circular wrap-around
-    valid_count++;
-    if (valid_count > MAX_LOG_ENTRIES) valid_count = MAX_LOG_ENTRIES;  // Ensure valid count does not exceed max
-}
+Logger<distanceLogSize> distanceLogger("distance_sensors");
+Logger<robotLogSize> robotLogger("robot");
+Logger<autonLogSize> autonLogger("auton");
+bool writingToFile = false;
 
 /**
  * Get the index of the log file
  * @return The index of the log file
  */
-static uint32_t get_log_file_idx() {
-    char read_buf[100]; // This just needs to be larger than the contents of the file
+static uint8_t get_log_file_idx() {
+    char read_buf[100]=""; // This just needs to be larger than the contents of the file
     FILE* index_file = fopen("/usd/index.txt", "r");
     if (index_file == NULL) {
         printf("Failed to open index file\n");
@@ -48,6 +24,9 @@ static uint32_t get_log_file_idx() {
     }
     fread(read_buf, 1, 50, index_file);
     fclose(index_file);
+    if (read_buf[0] == '\0') {
+        return 0;
+    }
     return std::stoi(read_buf);
 }
 
@@ -55,7 +34,7 @@ static uint32_t get_log_file_idx() {
  * Write the index of the log file
  * @param idx The index to write
  */
-static void write_log_file_idx(uint32_t idx) {
+static void write_log_file_idx(uint8_t idx) {
     FILE* index_file = fopen("/usd/index.txt", "w");
     if (index_file == NULL) {
         printf("Failed to open index file\n");
@@ -68,7 +47,9 @@ static void write_log_file_idx(uint32_t idx) {
 /**
  * Dump all logs from all instances to a single file
  */
-void Logger::dump_all() {
+void dump_all() {
+    writingToFile = true;
+    
     uint8_t idx = get_log_file_idx();
     idx++;
     write_log_file_idx(idx);
@@ -84,36 +65,9 @@ void Logger::dump_all() {
         return;
     } 
 
-    char log_string[256];
-
-    // Iterate over all registered loggers
-    for (Logger& logger : all_loggers) {
-        for (size_t i = 0; i < logger.valid_count; i++) {
-            const LogElement& log = logger.logs[i];
-
-            switch (log.type) {
-                case LogType::POSITION_EXPECTED:
-                    sprintf(log_string, "%s:, Expected, %f, %f, %f, %f\n", logger.logger_name.c_str(), log.time, log.a, log.b, log.c);
-                    break;
-                case LogType::POSITION_REAL:
-                    sprintf(log_string, "%s, Real, %f, %f, %f, %f\n", logger.logger_name.c_str(), log.time, log.a, log.b, log.c);
-                    break;
-                case LogType::LOOP_TIME:
-                    sprintf(log_string, "%s, LoopTime, %f, %f\n", logger.logger_name.c_str(), log.time, log.a);
-                    break;
-                case LogType::DISTANCE_SENSOR:
-                    sprintf(log_string, "%s, DistanceSensor, %f, %f, %f, %f, %f\n", logger.logger_name.c_str(), log.time, log.a, log.b, log.c, log.d);
-                    break;
-                case LogType::DELTA_MOVEMENT:
-                    sprintf(log_string, "%s, DeltaMovement, %f, %f, %f\n", logger.logger_name.c_str(), log.time, log.a, log.b);
-                    break;
-                default:
-                    break;
-            }
-
-            fputs(log_string, log_file);
-        }
-    }
+    distanceLogger.dump_file(log_file);
+    robotLogger.dump_file(log_file);
 
     fclose(log_file);
+    std::cout << "Dumped logs to file " << filename << std::endl;
 }
